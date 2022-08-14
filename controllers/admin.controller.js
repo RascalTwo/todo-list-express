ace3a8db47c0de87ffcbdc75a7870365ddddfaa7
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-13 20:17:32 -0400

    Convert multiple queries to single aggregation

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -2,13 +2,9 @@
 const db = require('../models/index.model.js');
 
 async function admin(request, response) {
-	const users = await db.db.collection('users').find().toArray();
-	for (const user of users) user.items = []
-	const items = await db.db.collection('todos').find().toArray();
-	for (const item of items){
-		const user = users.find(user => user._id.toString() === item.owner.toString());
-		user.items.push(item);
-	}
+	const users = await db.db.collection('users').aggregate([
+		{ $lookup: { from: 'todos', localField: '_id', foreignField: 'owner', as: 'items' } }
+	]).toArray();
 	response.render('admin.ejs', { users })
 }
 
